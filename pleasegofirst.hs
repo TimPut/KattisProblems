4924d4f3974acab912b531b8bb2368a600cadde8
timput <tim@timput.com>
2021-06-19 12:00:04 -0600

    pleasegofirst: didn't expect the naive solution to be so fast!

---
+++ pleasegofirst.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.ByteString.Char8 as B
+import Control.Monad
+import Data.Maybe
+import qualified Data.Map as M
+import Data.Char
+
+main = do
+  n <- fst . fromJust . B.readInt <$> B.getLine :: IO Int
+  results <- forM [1..n] runCase
+  B.putStrLn . B.unlines $ results
+
+runCase _ = do
+  k <- fst . fromJust . B.readInt <$> B.getLine :: IO Int
+  queue <- B.getLine :: IO B.ByteString
+  let result = (*5) . sum . concat . fmap rezero $ flip B.elemIndices (B.reverse queue) <$> cs
+  return (B.pack . show $ result)
+
+cs = filter isAlphaNum ['0'..'z']
+
+rezero [] = []
+rezero (i:is) = zipWith (-) (i:is) [i..]
